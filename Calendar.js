0432fbff730910f2361a095e6a1275f043c8bfe0
Checkmate Media <34839284+CheckmateMedia@users.noreply.github.com>
2020-08-15 12:46:02 -0600

    Add files via upload

--- Calendar.js
+++ Calendar.js
@@ -31,23 +31,145 @@ function myFunction() {
     var day = day[d.getDay()];
     var y = d.getFullYear();
 
+
+    x = new Date();
+    var date = x.getDate();
+    console.log(date)
+
+    //--------------------------------------------------------------------------
     document.getElementById("month").innerHTML = n;
     document.getElementById("month").style.fontWeight = "bold";
-    document.getElementById("month").style.fontSize = "1em";
+    document.getElementById("month").style.fontSize = "3em";
     document.getElementById("year").innerHTML = y;
     document.getElementById("year").style.fontWeight = "bold";
-    document.getElementById("year").style.fontSize = "1em";
+    document.getElementById("year").style.fontSize = "1.5em";
 
     console.log(n)
     console.log(y)
+    console.log(day)
+    //-------------------------------------------------------------------------------------------fontColor
+    if (date === 1) {
+        document.getElementById('aug1').style.color = "gold"
+        document.getElementById('aug1').style.fontSize = "2em"
+    }
+    if (date === 2) {
+        document.getElementById('aug2').style.color = "gold"
+        document.getElementById('aug2').style.fontSize = "2em"
+    }
+    if (date === 3) {
+        document.getElementById('aug3').style.color = "gold"
+        document.getElementById('aug3').style.fontSize = "2em"
+    }
+    if (date === 4) {
+        document.getElementById('aug4').style.color = "gold"
+        document.getElementById('aug4').style.fontSize = "2em"
+    }
+    if (date === 5) {
+        document.getElementById('aug5').style.color = "gold"
+        document.getElementById('aug5').style.fontSize = "2em"
+    }
+    if (date === 6) {
+        document.getElementById('aug6').style.color = "gold"
+        document.getElementById('aug6').style.fontSize = "2em"
+    }
+    if (date === 7) {
+        document.getElementById('aug7').style.color = "gold"
+        document.getElementById('aug7').style.fontSize = "2em"
+    }
+    if (date === 8) {
+        document.getElementById('aug8').style.color = "gold"
+        document.getElementById('aug8').style.fontSize = "2em"
+    }
+    if (date === 9) {
+        document.getElementById('aug9').style.color = "gold"
+        document.getElementById('aug9').style.fontSize = "2em"
+    }
+    if (date === 10) {
+        document.getElementById('aug10').style.color = "gold"
+        document.getElementById('aug10').style.fontSize = "2em"
+    }
+    if (date === 11) {
+        document.getElementById('aug11').style.color = "gold"
+        document.getElementById('aug11').style.fontSize = "2em"
+    }
+    if (date === 12) {
+        document.getElementById('aug12').style.color = "gold"
+        document.getElementById('aug12').style.fontSize = "2em"
+    }
+    if (date === 13) {
+        document.getElementById('aug13').style.color = "gold"
+        document.getElementById('aug15\3').style.fontSize = "2em"
+    }
+    if (date === 14) {
+        document.getElementById('aug14').style.color = "gold"
+        document.getElementById('aug14').style.fontSize = "2em"
+    }
+    if (date === 15) {
+        document.getElementById('aug15').style.color = "gold"
+        document.getElementById('aug15').style.fontSize = "2em"
+    }
+    if (date === 16) {
+        document.getElementById('aug16').style.color = "gold"
+        document.getElementById('aug16').style.fontSize = "2em"
+    }
+    if (date === 17) {
+        document.getElementById('aug17').style.color = "gold"
+        document.getElementById('aug17').style.fontSize = "2em"
+    }
+    if (date === 18) {
+        document.getElementById('aug18').style.color = "gold"
+        document.getElementById('aug18').style.fontSize = "2em"
+    }
+    if (date === 19) {
+        document.getElementById('aug19').style.color = "gold"
+        document.getElementById('aug19').style.fontSize = "2em"
+    }
+    if (date === 20) {
+        document.getElementById('aug21').style.color = "gold"
+        document.getElementById('aug22').style.fontSize = "2em"
+    }
+    if (date === 23) {
+        document.getElementById('aug23').style.color = "gold"
+        document.getElementById('aug24').style.fontSize = "2em"
+    }
+    if (date === 25) {
+        document.getElementById('aug25').style.color = "gold"
+        document.getElementById('aug25').style.fontSize = "2em"
+    }
+    if (date === 26) {
+        document.getElementById('aug26').style.color = "gold"
+        document.getElementById('aug26').style.fontSize = "2em"
+    }
+    if (date === 27) {
+        document.getElementById('aug27').style.color = "gold"
+        document.getElementById('aug27').style.fontSize = "2em"
+    }
+    if (date === 28) {
+        document.getElementById('aug28').style.color = "gold"
+        document.getElementById('aug28').style.fontSize = "2em"
+    }
+    if (date === 29) {
+        document.getElementById('aug29').style.color = "gold"
+        document.getElementById('aug29').style.fontSize = "2em"
+    }
+    if (date === 30) {
+        document.getElementById('aug30').style.color = "gold"
+        document.getElementById('aug30').style.fontSize = "2em"
+    }
+    if (date === 31) {
+        document.getElementById('aug31').style.color = "gold"
+        document.getElementById('aug31').style.fontSize = "2em"
+    }
 
-    //--------------------------------------------------------------------------Here's the issue
+    console.log(date)
+    console.log(date)
+    console.log(date)
+    //--------------------------------------------------------------------------
     if (n === "January") {
         document.getElementById("january").style.display = 'block'
     }
     else {
         document.getElementById("january").style.display = 'none'
-
     }
 
     if (n === "February") {
@@ -137,6 +259,8 @@ function myFunction() {
         document.getElementById("december").style.display = 'none'
 
     }
+
+
     //-----------------------------------------------------------------------------------
 
 
