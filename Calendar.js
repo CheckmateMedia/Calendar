209a84d4495c44d2415419330c83d68c18b81456
Checkmate Media <34839284+CheckmateMedia@users.noreply.github.com>
2020-08-16 11:08:03 -0600

    Add files via upload

--- Calendar.js
+++ Calendar.js
@@ -1763,157 +1763,157 @@ function myFunction() {
     if (date === 1 && (n === "December")) {
         document.getElementById('dec1').style.color = "gold"
         document.getElementById('dec1').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 2 && (n === "December")) {
         document.getElementById('dec2').style.color = "gold"
         document.getElementById('dec2').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 3 && (n === "December")) {
         document.getElementById('dec3').style.color = "gold"
         document.getElementById('dec3').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 4 && (n === "December")) {
         document.getElementById('dec4').style.color = "gold"
         document.getElementById('dec4').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 5 && (n === "December")) {
         document.getElementById('dec5').style.color = "gold"
         document.getElementById('dec5').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 6 && (n === "December")) {
         document.getElementById('dec6').style.color = "gold"
         document.getElementById('dec6').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 7 && (n === "December")) {
         document.getElementById('dec7').style.color = "gold"
         document.getElementById('dec7').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 8 && (n === "December")) {
         document.getElementById('dec8').style.color = "gold"
         document.getElementById('dec8').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 9 && (n === "December")) {
         document.getElementById('dec9').style.color = "gold"
         document.getElementById('dec9').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 10 && (n === "December")) {
         document.getElementById('dec10').style.color = "gold"
         document.getElementById('dec10').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 11 && (n === "December")) {
         document.getElementById('dec11').style.color = "gold"
         document.getElementById('dec11').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 12 && (n === "December")) {
         document.getElementById('dec12').style.color = "gold"
         document.getElementById('dec12').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 13 && (n === "December")) {
         document.getElementById('dec13').style.color = "gold"
         document.getElementById('dec15').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 14 && (n === "December")) {
         document.getElementById('dec14').style.color = "gold"
         document.getElementById('dec14').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 15 && (n === "December")) {
         document.getElementById('dec15').style.color = "gold"
         document.getElementById('dec15').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 16 && (n === "December")) {
         document.getElementById('dec16').style.color = "gold"
         document.getElementById('dec16').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 17 && (n === "December")) {
         document.getElementById('dec17').style.color = "gold"
         document.getElementById('dec17').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 18 && (n === "December")) {
         document.getElementById('dec18').style.color = "gold"
         document.getElementById('dec18').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 19 && (n === "December")) {
         document.getElementById('dec19').style.color = "gold"
         document.getElementById('dec19').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 20 && (n === "December")) {
         document.getElementById('dec20').style.color = "gold"
         document.getElementById('dec20').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 21 && (n === "December")) {
         document.getElementById('dec21').style.color = "gold"
         document.getElementById('dec21').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 22 && (n === "December")) {
         document.getElementById('dec22').style.color = "gold"
         document.getElementById('dec22').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 23 && (n === "December")) {
         document.getElementById('dec23').style.color = "gold"
         document.getElementById('dec24').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 24 && (n === "December")) {
         document.getElementById('dec24').style.color = "gold"
         document.getElementById('dec24').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 25 && (n === "December")) {
         document.getElementById('dec25').style.color = "gold"
         document.getElementById('dec25').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 26 && (n === "December")) {
         document.getElementById('dec26').style.color = "gold"
         document.getElementById('dec26').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 27 && (n === "December")) {
         document.getElementById('dec27').style.color = "gold"
         document.getElementById('dec27').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 28 && (n === "December")) {
         document.getElementById('dec28').style.color = "gold"
         document.getElementById('dec28').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 29 && (n === "December")) {
         document.getElementById('dec29').style.color = "gold"
         document.getElementById('dec29').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 30 && (n === "December")) {
         document.getElementById('dec30').style.color = "gold"
         document.getElementById('dec30').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
     if (date === 31 && (n === "December")) {
         document.getElementById('dec31').style.color = "gold"
         document.getElementById('dec31').style.fontSize = "2em"
-        document.body.style.backgroundColor = "#ff6a57";
+        document.body.style.backgroundColor = "#76ff63";
     }
 
     //----------------------------------------------------------------------------------
@@ -2018,4 +2018,22 @@ function myFunction() {
 
 
 
+
+
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+        h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
 }
